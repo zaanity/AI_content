52877b4af816164eba2ab719da463904050a42bd
mathias-grdl <114570081+mathias-grdl@users.noreply.github.com>
2024-06-27 21:10:38 +0200

    add rich text editor for output

--- app/dashboard/content/[template-slug]/_components/OutputSection/index.tsx
+++ app/dashboard/content/[template-slug]/_components/OutputSection/index.tsx
@@ -1,5 +1,30 @@
-import React from "react";
+import { Button } from "@/components/ui/button";
+import { Copy } from "lucide-react";
+import React, { useRef } from "react";
+require("@toast-ui/editor/dist/toastui-editor.css");
+const { Editor } = require("@toast-ui/react-editor");
 
 export default function OutputSection() {
-	return <div>OutputSection</div>;
+	const editorRef: any = useRef();
+
+	return (
+		<div className="bg-white shadow-lg border rounded-lg">
+			<div className="flex justify-between items-center p-5">
+				<h2 className="font-medium text-lg">Your Result</h2>
+				<Button className="flex gap-2 items-center">
+					<Copy className="w-4 h-4" /> Copy
+				</Button>
+			</div>
+			<Editor
+				ref={editorRef}
+				initialValue="result"
+				initialEditType="markdown"
+				height="600px"
+				useCommandShortcut={true}
+				onChange={() =>
+					console.log(editorRef.current.getInstance().getMarkdown())
+				}
+			/>
+		</div>
+	);
 }
