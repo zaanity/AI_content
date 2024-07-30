3f93b60b41af31af1cfed672b9a38940f150c613
zaanity <121CS0186@nitrkl.ac.in>
2024-07-30 21:18:16 +0530

    new update

--- app/page.tsx
+++ app/page.tsx
@@ -49,29 +49,7 @@ export default function Home() {
 			<div className="relative overflow-hidden before:absolute before:top-0 before:start-1/2 before:bg-[url('https://preline.co/assets/svg/examples/polygon-bg-element.svg')] dark:before:bg-[url('https://preline.co/assets/svg/examples-dark/polygon-bg-element.svg')] before:bg-no-repeat before:bg-top before:bg-cover before:size-full before:-z-[1] before:transform before:-translate-x-1/2">
 				<div className="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 pt-24 pb-10">
 					<div className="flex justify-center">
-						<a
-							className="inline-flex items-center gap-x-2 bg-white border border-gray-200 text-sm text-gray-800 p-1 ps-3 rounded-full transition hover:border-gray-300 dark:bg-neutral-800 dark:border-neutral-700 dark:hover:border-neutral-600 dark:text-neutral-200"
-							href="https://mathias-grondziel.com/"
-							target="_blank"
-						>
-							Mini project by Mathias
-							<span className="py-1.5 px-2.5 inline-flex justify-center items-center gap-x-2 rounded-full bg-gray-200 font-semibold text-sm text-gray-600 dark:bg-neutral-700 dark:text-neutral-400">
-								<svg
-									className="flex-shrink-0 size-4"
-									xmlns="http://www.w3.org/2000/svg"
-									width="24"
-									height="24"
-									viewBox="0 0 24 24"
-									fill="none"
-									stroke="currentColor"
-									stroke-width="2"
-									stroke-linecap="round"
-									stroke-linejoin="round"
-								>
-									<path d="m9 18 6-6-6-6" />
-								</svg>
-							</span>
-						</a>
+						{/*  */}
 					</div>
 
 					<div className="mt-5 max-w-2xl text-center mx-auto">
--- drizzle.config.js
+++ drizzle.config.js
@@ -3,6 +3,6 @@ export default {
 	schema: "./utils/Backend/Schema/index.tsx",
 	dialect: "postgresql",
 	dbCredentials: {
-		url: "postgresql://AI-Content-Generator_owner:2mwurWgyT8Ea@ep-wild-star-a2qp5yvz.eu-central-1.aws.neon.tech/AI-Content-Generator?sslmode=require",
+		url: "postgresql://AI-Content-Generator_owner:2uMQJ3DzFIdr@ep-bold-base-a1omjxzn-pooler.ap-southeast-1.aws.neon.tech/AI-CONTENT?sslmode=require",
 	},
 };
